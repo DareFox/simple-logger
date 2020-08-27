7611e04aed7038f2b17caede92d02ceb9495f4c5
DareFox <47672780+DareFox@users.noreply.github.com>
2020-08-27 18:59:13 +0300

    Logger itself

---
+++ index.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const fs = require('fs');
+const colors = require('colors');
+const dateFormat = require('dateformat');
+const root = path.dirname(require.main.filename)
+
+function write(file, text) {
+  if (fs.existsSync(`${root}/log`) == false) {
+    fs.mkdirSync(`${root}/log`)
+  }
+
+  if (fs.existsSync(`${root}/log/${file}` == false)) {
+    fs.createFileSync(file);
+  }
+
+  fs.appendFileSync(file, text);
+}
+
+function dateFull() {
+  const date = new Date();
+  const dateFull = dateFormat(date, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
+  return dateFull;
+}
+
+let date, hour, minute, second, dateDMY;
+
+date = new Date();
+hour = dateFormat(date, 'hh');
+minute = dateFormat(date, 'MM'); // MM - For minutes
+second = dateFormat(date, 'ss');
+dateDMY = dateFormat(date, 'dd-mm-yyyy'); // mm - For month
+
+const pathToLog = `${root}/log/date-${dateDMY}-time-${hour}h-${minute}m-${second}s-bot.log`;
+
+module.exports = {
+  info: (text) => {
+    const log = `[${dateFull()} - INFO]: ${text}\n`;
+    console.log(log);
+    write(pathToLog, log);
+  },
+  warn: (text) => {
+    const log = `[${dateFull()} - WARN]: ${text}\n`;
+    console.log(log.yellow);
+    write(pathToLog, log);
+  },
+  err: (text, error_code) => {
+    const log = `[${dateFull()} - ERROR (CODE: "${error_code}")]: ${text}\n`;
+    console.log(log.red);
+    write(pathToLog, log);
+  },
+  trace: (text) => {
+    const log = `[${dateFull()} - TRACE]: ${text}\n`;
+    console.log(log.blue);
+    write(pathToLog, log);
+  }
+};
